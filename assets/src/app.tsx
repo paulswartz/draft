5de6bcdfa47a58e47fb63e43fb6194674d5165f1
Brady <kbrady@CTD24877KBRADY.local>
2021-05-04 16:16:20 -0400

    Setup to use typscript, following Arrow

---
+++ assets/src/app.tsx
@@ -0,0 +1,8 @@
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+declare function require(name: string): string
+// tslint:disable-next-line
+require("../css/app.scss")
+
+import "phoenix_html"
