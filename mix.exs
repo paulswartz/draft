97e0f0a3b541ba73ca454f87e8e1d0f792de01e8
Brady <kbrady@CTD24877KBRADY.local>
2021-05-13 10:44:22 -0400

    Add lcov dependency

--- mix.exs
+++ mix.exs
@@ -48,6 +48,7 @@ defmodule Draft.MixProject do
       {:phoenix, "~> 1.5.8"},
       {:phoenix_ecto, "~> 4.1"},
       {:ecto_sql, "~> 3.4"},
+      {:lcov_ex, "~> 0.1.1", only: :test, runtime: false},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
