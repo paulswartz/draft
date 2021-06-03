edbc04561f6c42bd91209c70a23bb58ad8a2c022
Brady <kbrady@CTD24877KBRADY.local>
2021-06-03 12:44:45 -0400

    Fix tests

--- test/draft/employee_vacation_assignment_test.exs
+++ test/draft/employee_vacation_assignment_test.exs
@@ -4,31 +4,16 @@ defmodule Draft.EmployeeVacationAssignmentTest do
 
   describe "to_csv_row/1" do
     test "correct values" do
-      assert [
-               [
-                 "vacation",
-                 ?|,
-                 "0001",
-                 ?|,
-                 "1",
-                 ?|,
-                 "01/01/2021",
-                 ?|,
-                 "01/08/2021",
-                 ?|,
-                 "1",
-                 ?|,
-                 "1",
-                 ?\n
-               ]
-             ] =
-               EmployeeVacationAssignment.to_csv_row(%EmployeeVacationAssignment{
-                 employee_id: "0001",
-                 start_date: ~D[2021-01-01],
-                 end_date: ~D[2021-01-08],
-                 forced?: true,
-                 vacation_interval_type: "1"
-               })
+      assert "vacation|0001|1|01/01/2021|01/08/2021|1|1\n" =
+               IO.iodata_to_binary(
+                 EmployeeVacationAssignment.to_csv_row(%EmployeeVacationAssignment{
+                   employee_id: "0001",
+                   start_date: ~D[2021-01-01],
+                   end_date: ~D[2021-01-08],
+                   forced?: true,
+                   vacation_interval_type: "1"
+                 })
+               )
     end
   end
 end
--- test/draft/formatting_helpers_test.exs
+++ test/draft/formatting_helpers_test.exs
@@ -4,11 +4,11 @@ defmodule Draft.FormattingHelpersTest do
 
   describe "to_date_string/1" do
     test "Month less than 10" do
-      assert ParsingHelpers.to_date_string(~D[2034-01-12]) == "01/12/2034"
+      assert FormattingHelpers.to_date_string(~D[2034-01-12]) == "01/12/2034"
     end
 
     test "Month greater than 10" do
-      assert ParsingHelpers.to_date_string(~D[2034-11-12]) == "11/12/2034"
+      assert FormattingHelpers.to_date_string(~D[2034-11-12]) == "11/12/2034"
     end
   end
 end
