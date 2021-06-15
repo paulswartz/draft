8abd8e2ff2a8babb146af41bfd6aa48e858c134b
Brady <kbrady@CTD24877KBRADY.local>
2021-06-15 11:58:45 -0400

    Basic rendering of days / weeks

--- assets/src/vacation/vacationQuotaIndex.tsx
+++ assets/src/vacation/vacationQuotaIndex.tsx
@@ -1,14 +1,22 @@
 import * as React from "react"
-import { fetchAvailableVacationQuotas, VacationQuotaSummary } from "../ap"
+import { fetchAvailableVacationQuotas, VacationQuotaSummary, VacationDayQuota, VacationWeekQuota } from "../ap"
 import { useEffect, useState } from "react"
 
+
+const VacationDayDisplay = (day: VacationDayQuota): JSX.Element => {
+    return <p>{day.date}, quota: {day.quota}</p>
+}
+
+const VacationWeekDisplay = (week: VacationWeekQuota): JSX.Element => {
+    return <p>{week.start_date} through {week.end_date}, quota: {week.quota}</p>
+}
 
 const VacationQuotaIndex = (): JSX.Element => {
     const useVacationQuotaSummary = (): VacationQuotaSummary | null => {
         const [availableVacationQuotas, setAvailableVacationQuotas] = useState<VacationQuotaSummary| null>(null)
         useEffect(() => {
-          fetchAvailableVacationQuotas().then().then(setAvailableVacationQuotas)
-        })
+          fetchAvailableVacationQuotas().then(setAvailableVacationQuotas)
+        }, [])
         return availableVacationQuotas
       }
 
@@ -16,7 +24,13 @@ const VacationQuotaIndex = (): JSX.Element => {
 
     
       
-    return <p>There are {availQuota?.days.length} days</p>
+    return <div> <h3>Days</h3>
+    {<p>{availQuota?.days.map(day => {return VacationDayDisplay(day)})}</p>}
+    <h3>Weeks</h3>
+    {<p>{availQuota?.weeks.map(week => {return VacationWeekDisplay(week)})}</p>}
+    </div>
 }
+
+
 
 export default VacationQuotaIndex
