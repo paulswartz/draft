d5276c5ca4859d09943e926122c680360a265378
Brady <kbrady@CTD24877KBRADY.local>
2021-06-15 14:36:13 -0400

    Basic selection mechanism working

--- assets/src/vacation/vacationQuotaIndex.tsx
+++ assets/src/vacation/vacationQuotaIndex.tsx
@@ -1,34 +1,59 @@
 import * as React from "react"
-import { fetchAvailableVacationQuotas, VacationQuotaSummary, VacationDayQuota, VacationWeekQuota } from "../ap"
+import { fetchAvailableVacationQuotas, VacationQuotaSummary, VacationDayQuota, VacationWeekQuota } from "../api"
 import { useEffect, useState } from "react"
 
-
-const VacationDayDisplay = (day: VacationDayQuota): JSX.Element => {
-    return <p>{day.date}, quota: {day.quota}</p>
-}
-
-const VacationWeekDisplay = (week: VacationWeekQuota): JSX.Element => {
-    return <p>{week.start_date} through {week.end_date}, quota: {week.quota}</p>
-}
-
 const VacationQuotaIndex = (): JSX.Element => {
-    const useVacationQuotaSummary = (): VacationQuotaSummary | null => {
-        const [availableVacationQuotas, setAvailableVacationQuotas] = useState<VacationQuotaSummary| null>(null)
-        useEffect(() => {
-          fetchAvailableVacationQuotas().then(setAvailableVacationQuotas)
-        }, [])
-        return availableVacationQuotas
-      }
+  const [selectedWeeks, setSelectedWeeks] = useState<String[] | []>([])
+  const [selectedDays, setSelectedDays] = useState<String[] | []>([])
 
-    const availQuota: VacationQuotaSummary | null = useVacationQuotaSummary()
+  const handleWeekInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.checked ?
+      setSelectedWeeks([...selectedWeeks, event.target.value])
+      : setSelectedWeeks(selectedWeeks.filter(week => week !== event.target.value))
+  }
 
-    
-      
-    return <div> <h3>Days</h3>
-    {<p>{availQuota?.days.map(day => {return VacationDayDisplay(day)})}</p>}
+
+  const handleDayInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    event.target.checked ?
+      setSelectedDays([...selectedDays, event.target.value])
+      : setSelectedDays(selectedDays.filter(day => day !== event.target.value))
+  }
+
+  const VacationDayDisplay = (day: VacationDayQuota): JSX.Element => {
+    return <div><input type="checkbox" value={day.date} onChange={(e) => handleDayInputChange(e)}></input> <p>{day.date}, quota: {day.quota}</p></div>
+  }
+
+  const VacationWeekDisplay = (week: VacationWeekQuota): JSX.Element => {
+    return <div><input type="checkbox" value={week.start_date.toString()} onChange={(e) => handleWeekInputChange(e)} ></input> <p> week of {week.start_date}, quota: {week.quota}</p></div>
+  }
+
+
+
+  const useVacationQuotaSummary = (): VacationQuotaSummary | null => {
+    const [availableVacationQuotas, setAvailableVacationQuotas] = useState<VacationQuotaSummary | null>(null)
+    useEffect(() => {
+      fetchAvailableVacationQuotas().then(setAvailableVacationQuotas)
+    }, [])
+    return availableVacationQuotas
+  }
+
+  const availQuota: VacationQuotaSummary | null = useVacationQuotaSummary()
+
+
+
+  return <div>
+    <h3>Selections</h3>
+    <h4>Weeks</h4>
+    {selectedWeeks.map(week => { return <p>{week}</p> })}
+    <h4>Days</h4>
+    {selectedDays.map(day => { return <p>{day}</p> })}
     <h3>Weeks</h3>
-    {<p>{availQuota?.weeks.map(week => {return VacationWeekDisplay(week)})}</p>}
-    </div>
+    {availQuota?.weeks.map(week => { return VacationWeekDisplay(week) })}
+
+    <h3>Days</h3>
+    {availQuota?.days.map(day => { return VacationDayDisplay(day) })}
+
+  </div>
 }
 
 
