2fa0737c9c4f92710303cd89d0b3279ab2c2c410
Brady <kbrady@CTD24877KBRADY.local>
2021-06-22 14:10:45 -0400

    Rough use of a reducer

--- assets/src/components/vacationPreferenceForm.tsx
+++ assets/src/components/vacationPreferenceForm.tsx
@@ -6,42 +6,66 @@ import {
 } from "../models/divisionVacationQuotaData";
 import useDivisionAvailableVacationQuotas from "../hooks/useDivisionAvailableVacationQuotas";
 import {apiSend, fetchVacationPreferenceSet} from "../api"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useReducer } from "react";
+
+function init() {
+  return fetchVacationPreferenceSet()
+  .then((prefs) => {if (prefs != null) return {weeks: (prefs.weeks.map(pref => (pref.start_date.toString()))), days: prefs.days.map(pref => (pref.start_date.toString()))};
+  else return {weeks: [], days: []}})
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'set_weeks':
+      return  { 
+        ...state,
+        weeks: action.payload
+    }
+    case 'set_days':
+      return  { 
+        ...state,
+        days: action.payload
+    }
+    case "initial_load":
+    return {weeks: action.payload.weeks, days: action.payload.days}
+    default:
+      throw new Error();
+  }
+}
+
+
 
 const VacationPreferenceForm = (): JSX.Element => {
-  const [selectedWeeks, setSelectedWeeks] = useState<String[]>([]);
-  const [selectedDays, setSelectedDays] = useState<String[]>([]);
+
+  const [state, dispatch] = useReducer(reducer, {weeks: [], days: []});
 
   const handleWeekInputChange = (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const updatedWeekPreferences = 
     event.target.checked
-      ? [...selectedWeeks, event.target.value]
+      ? [...state.weeks, event.target.value]
       : 
-          selectedWeeks.filter((week) => week !== event.target.value)
+          state.weeks.filter((week) => week !== event.target.value)
         ;
-    setSelectedWeeks(updatedWeekPreferences)
+        const ranked_weeks = updatedWeekPreferences.map((pref, index) => ({start_date: pref, rank: index + 1}));
+        const ranked_days = state.days.map((pref, index) => ({start_date: pref, rank: index + 1}));
+      apiSend({url: "/api/vacation/preferences", method: "POST", json: JSON.stringify({weeks: ranked_weeks, days: ranked_days})})
+      dispatch({type:'set_weeks', payload: updatedWeekPreferences })
   };
 
   useEffect(() => {
-  fetchVacationPreferenceSet().then((prefs) => {if (prefs != null) {setSelectedWeeks(prefs.weeks.map(pref => (pref.start_date.toString()))); setSelectedDays(prefs.days.map(pref => (pref.start_date.toString()))) }})
-}, []);
+    fetchVacationPreferenceSet().then((prefs) => {if (prefs != null) {dispatch({type: "initial_load", payload: {weeks: (prefs.weeks.map(pref => (pref.start_date.toString()))), days: prefs.days.map(pref => (pref.start_date.toString()))}})}});
+  }, []);
 
-
-    useEffect(() => {
-      const ranked_weeks = selectedWeeks.map((pref, index) => ({start_date: pref, rank: index + 1}));
-      const ranked_days = selectedDays.map((pref, index) => ({start_date: pref, rank: index + 1}));
-    apiSend({url: "/api/vacation/preferences", method: "POST", json: JSON.stringify({weeks: ranked_weeks, days: ranked_days})})
-  }, [selectedWeeks, selectedDays]);
-  
 
   const handleDayInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    event.target.checked
+  /*  event.target.checked
       ? setSelectedDays([...selectedDays, event.target.value])
       : setSelectedDays(
           selectedDays.filter((day) => day !== event.target.value)
         );
+        */
   };
 
   const VacationDayDisplay = (day: VacationDayQuotaData): JSX.Element => {
@@ -77,16 +101,32 @@ const VacationPreferenceForm = (): JSX.Element => {
   const availQuota: DivisionAvailableVacationQuotaData | null =
     useDivisionAvailableVacationQuotas();
 
+/*   return (
+    <div>
+      <h3>Preferred Vacation</h3>
+      <h4>Weeks</h4>
+    <ul>{state.weeks.map((week) => (
+        <li key={week.toString()}>{week}</li>
+      ))}</ul>
+      <h4>Days</h4>
+      <ul>{state.weeks.map((day) => (
+        <li key={day.toString()}>{day}</li>
+      ))}</ul>
+      <h3>Available Vacation Time</h3>
+      <h4>Weeks</h4>
+      {availQuota?.weeks.map((week) => VacationWeekDisplay(week))}
+
+      <h4>Days</h4>
+      {availQuota?.days.map((day) => VacationDayDisplay(day))}
+    </div>
+  ); */
+  console.log(state)
   return (
     <div>
       <h3>Preferred Vacation</h3>
       <h4>Weeks</h4>
-      <ul>{selectedWeeks.map((week) => (
+    <ul>{state.weeks.map((week) => (
         <li key={week.toString()}>{week}</li>
-      ))}</ul>
-      <h4>Days</h4>
-      <ul>{selectedDays.map((day) => (
-        <li key={day.toString()}>{day}</li>
       ))}</ul>
       <h3>Available Vacation Time</h3>
       <h4>Weeks</h4>
