40b2b8f13a29a17e22bd44a8791b714d7e87f702
Brady <kbrady@CTD24877KBRADY.local>
2021-07-29 12:17:00 -0400

    formatting

--- priv/repo/migrations/20210729154718_remove_preference_set_fk.exs
+++ priv/repo/migrations/20210729154718_remove_preference_set_fk.exs
@@ -2,6 +2,9 @@ defmodule Draft.Repo.Migrations.RemovePreferenceSetFk do
   use Ecto.Migration
 
   def change do
-    drop constraint(:employee_vacation_preference_sets, "employee_vacation_preference_sets_round_id_fkey")
+    drop constraint(
+           :employee_vacation_preference_sets,
+           "employee_vacation_preference_sets_round_id_fkey"
+         )
   end
 end
