7c477d88ba3ec82150163bf3eb0a2efa32c87f86
Brady <kbrady@CTD24877KBRADY.local>
2021-08-17 16:20:11 -0400

    feat: limit the vacation for operators voluntarily picking to their preferences

--- lib/draft/generate_vacation_distribution/days.ex
+++ lib/draft/generate_vacation_distribution/days.ex
@@ -87,115 +87,51 @@ defmodule Draft.GenerateVacationDistribution.Days do
        )
 
   defp generate_from_available(
-         _distribution_run_id,
-         _round,
-         _employee,
-         0
-       ) do
-    Logger.info(
-      "Skipping vacation day assignment - employee cannot take any days off in this rating period."
-    )
-
-    []
-  end
-
-  defp generate_from_available(
          distribution_run_id,
          round,
          employee,
          max_days
        ) do
-    preference_set =
-      Draft.EmployeeVacationPreferenceSet.latest_preference_set(
-        employee.process_id,
-        employee.round_id,
-        employee.employee_id,
-        [:day]
-      )
+        preferred_days =
+          distribution_run_id
+          |> ranked_available_days(round, employee)
+          |> Enum.take(max_days)
+          |> Enum.filter(& &1.preference_rank)
 
-    all_available_days = get_all_days_available_to_employee(distribution_run_id, round, employee)
-
-    preferred_vacation_days =
-      if is_nil(preference_set) do
-        []
-      else
-        Enum.filter(preference_set.vacation_preferences, fn p -> p.interval_type == :day end)
-      end
-
-    generate_days_to_distribute_from_preferences(
-      employee,
-      all_available_days,
-      preferred_vacation_days,
-      max_days
-    )
+        Enum.map(preferred_days, &to_distribution(employee, &1))
   end
 
-  defp get_all_days_available_to_employee(distribution_run_id, round, employee) do
+  defp ranked_available_days(
+         distribution_run_id,
+         round,
+         employee
+       ) do
     quota_already_distributed_in_run =
       Draft.VacationDistribution.count_unsynced_assignments_by_date(distribution_run_id, :day)
 
     round
-    |> DivisionVacationDayQuota.available_quota(employee)
+    |> Draft.DivisionQuotaRanked.available_to_employee(employee, :day)
     |> Enum.map(fn original_quota ->
-      %DivisionVacationDayQuota{
+      %{
         original_quota
         | quota:
             original_quota.quota -
-              Map.get(quota_already_distributed_in_run, original_quota.date, 0)
+              Map.get(quota_already_distributed_in_run, original_quota.start_date, 0)
       }
     end)
     |> Enum.filter(fn q -> q.quota > 0 end)
   end
 
-  defp generate_days_to_distribute_from_preferences(
-         employee,
-         all_available_days,
-         preferred_days,
-         max_days
-       )
-
-  defp generate_days_to_distribute_from_preferences(employee, all_available_days, [], max_days) do
-    generate_days(employee, Enum.take(all_available_days, max_days))
-  end
-
-  defp generate_days_to_distribute_from_preferences(
-         employee,
-         all_available_days,
-         preferred_days,
-         max_days
-       ) do
-    available_days_set = MapSet.new(all_available_days, fn d -> d.date end)
-
-    available_preferred_days =
-      preferred_days
-      |> Enum.filter(fn preferred_day ->
-        MapSet.member?(available_days_set, preferred_day.start_date)
-      end)
-      |> Enum.take(max_days)
-      |> Enum.map(fn d -> %{date: d.start_date} end)
-
-    generate_days(employee, available_preferred_days)
-  end
-
-  defp generate_days(employee, available_days)
-
-  defp generate_days(_employee, []) do
-    Logger.info("No more vacation days available")
-    []
-  end
-
-  defp generate_days(employee, available_days) do
-    Enum.map(available_days, &generate_day(employee, &1))
-  end
-
-  defp generate_day(employee, selected_day) do
-    Logger.info("assigned day - #{selected_day.date}")
+  defp to_distribution(employee, assigned_day) do
+    Logger.info("assigned day - #{assigned_day.start_date}")
 
     %VacationDistribution{
       employee_id: employee.employee_id,
       interval_type: :day,
-      start_date: selected_day.date,
-      end_date: selected_day.date
+      start_date: assigned_day.start_date,
+      end_date: assigned_day.end_date,
+      preference_rank: assigned_day.preference_rank,
+      is_forced: false
     }
   end
 end
--- lib/draft/generate_vacation_distribution/weeks.ex
+++ lib/draft/generate_vacation_distribution/weeks.ex
@@ -1,19 +1,25 @@
 defmodule Draft.GenerateVacationDistribution.Weeks do
   @moduledoc """
   Generate a list of vacation weeks that can be assigned to the given employee
+  based on their preferences
   """
-  @behaviour Draft.GenerateVacationDistribution.Voluntary
-  alias Draft.DivisionVacationWeekQuota
-  alias Draft.GenerateVacationDistribution.Voluntary
   alias Draft.VacationDistribution
-
   require Logger
 
+  @spec generate(
+          integer(),
+          Draft.BidRound.t(),
+          Draft.EmployeeRanking.t(),
+          integer(),
+          nil | %{anniversary_date: Date.t(), anniversary_weeks: number()}
+        ) :: [VacationDistribution.t()]
+
   @doc """
-  generate a list of vacation weeks for an employee based on what is available in their division/job class in the rating period they are picking for.
-  If the employee has an upcoming anniversary date, vacation weeks are only generated up to that date.
+  generate a list of vacation weeks for an employee based on their preferences and what is
+  still available in their division/job class in the rating period they are picking for.
+  If the employee has an upcoming anniversary date, distributions will be generated only
+  for the amount of quota earned prior to the anniversary.
   """
-  @impl Voluntary
   def generate(
         distribution_run_id,
         round,
@@ -80,62 +86,16 @@ defmodule Draft.GenerateVacationDistribution.Weeks do
          employee,
          max_weeks
        ) do
-    preference_set =
-      Draft.EmployeeVacationPreferenceSet.latest_preference_set(
-        employee.process_id,
-        employee.round_id,
-        employee.employee_id,
-        [:week]
-      )
-
-    all_available_weeks =
-      get_all_weeks_available_to_employee(distribution_run_id, round, employee)
-
-    preferred_vacation_weeks =
-      if is_nil(preference_set) do
-        []
-      else
-        Enum.filter(preference_set.vacation_preferences, fn p -> p.interval_type == :week end)
-      end
-
-    generate_weeks_to_distribute_from_preferences(
-      employee,
-      all_available_weeks,
-      preferred_vacation_weeks,
-      max_weeks
-    )
-  end
-
-  defp generate_weeks_to_distribute_from_preferences(
-         employee,
-         all_available_weeks,
-         preferred_weeks,
-         max_weeks
-       )
-
-  defp generate_weeks_to_distribute_from_preferences(employee, all_available_weeks, [], max_weeks) do
-    generate_weeks(employee, Enum.take(all_available_weeks, max_weeks))
-  end
-
-  defp generate_weeks_to_distribute_from_preferences(
-         employee,
-         all_available_weeks,
-         preferred_weeks,
-         max_weeks
-       ) do
-    available_weeks_set = MapSet.new(all_available_weeks, fn w -> {w.start_date, w.end_date} end)
-
-    available_preferred_weeks =
-      preferred_weeks
-      |> Enum.filter(fn preferred_week ->
-        MapSet.member?(available_weeks_set, {preferred_week.start_date, preferred_week.end_date})
-      end)
+    preferred_weeks =
+      distribution_run_id
+      |> ranked_available_weeks(round, employee)
       |> Enum.take(max_weeks)
+      |> Enum.filter(& &1.preference_rank)
 
-    generate_weeks(employee, available_preferred_weeks)
+    Enum.map(preferred_weeks, &to_distribution(employee, &1))
   end
 
-  defp get_all_weeks_available_to_employee(
+  defp ranked_available_weeks(
          distribution_run_id,
          round,
          employee
@@ -144,9 +104,9 @@ defmodule Draft.GenerateVacationDistribution.Weeks do
       Draft.VacationDistribution.count_unsynced_assignments_by_date(distribution_run_id, :week)
 
     round
-    |> DivisionVacationWeekQuota.available_quota(employee)
+    |> Draft.DivisionQuotaRanked.available_to_employee(employee, :week)
     |> Enum.map(fn original_quota ->
-      %DivisionVacationWeekQuota{
+      %{
         original_quota
         | quota:
             original_quota.quota -
@@ -156,25 +116,16 @@ defmodule Draft.GenerateVacationDistribution.Weeks do
     |> Enum.filter(fn q -> q.quota > 0 end)
   end
 
-  defp generate_weeks(employee, available_weeks)
-
-  defp generate_weeks(_employee, []) do
-    Logger.info("No more vacation weeks available")
-    []
-  end
-
-  defp generate_weeks(employee, available_weeks) do
-    Enum.map(available_weeks, &generate_week(employee, &1))
-  end
-
-  defp generate_week(employee, assigned_week) do
+  defp to_distribution(employee, assigned_week) do
     Logger.info("assigned week - #{assigned_week.start_date} - #{assigned_week.end_date}")
 
     %VacationDistribution{
       employee_id: employee.employee_id,
       interval_type: :week,
       start_date: assigned_week.start_date,
-      end_date: assigned_week.end_date
+      end_date: assigned_week.end_date,
+      preference_rank: assigned_week.preference_rank,
+      is_forced: false
     }
   end
 end
