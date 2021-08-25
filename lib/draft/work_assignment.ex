f2c8d6c645e21f987b83655f8d481fd03e495669
Brady <kbrady@CTD24877KBRADY.local>
2021-08-25 13:52:01 -0400

    refactor: Lookup hours worked in from_parts

--- lib/draft/work_assignment.ex
+++ lib/draft/work_assignment.ex
@@ -1,6 +1,9 @@
 defmodule Draft.WorkAssignment do
   @moduledoc """
-  Represents the work that an operator is doing on a particular day
+  Represents the work that an operator is doing on a particular day.
+  The `hours_worked` field can be nil in the case where we don't yet know an
+  operator's assignment on a particular day -- for example, if they have chosen
+  to work VR, but have not yet chosen
   """
   @behaviour Draft.Parsable
   use Ecto.Schema
@@ -58,7 +61,7 @@ defmodule Draft.WorkAssignment do
       _operating_days
     ] = row
 
-    %__MODULE__{
+    assignment_from_file = %__MODULE__{
       employee_id: employee_id,
       is_dated_exception: Draft.ParsingHelpers.to_boolean(is_dated_exception),
       operating_date: Draft.ParsingHelpers.to_date(operating_date),
@@ -75,6 +78,56 @@ defmodule Draft.WorkAssignment do
         |> String.split(",")
         |> List.first()
         |> Draft.ParsingHelpers.to_optional_integer()
+    }
+
+    work_assignment_with_hours(assignment_from_file)
+  end
+
+  @spec work_assignment_with_hours(Draft.WorkAssignment.t()) :: Draft.WorkAssignment.t()
+
+  # Return a work assignment with the `hours_worked` field populated
+  # based on the job class, assignment, and duty. If there is not enough information
+  # to determine the hours worked (ex: assignment is VR), hours_worked will be nil.
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "VR"} = work_assignment) do
+    %{work_assignment | hours_worked: nil}
+  end
+
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "LR08"} = work_assignment) do
+    %{work_assignment | hours_worked: 8}
+  end
+
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "LR10"} = work_assignment) do
+    %{work_assignment | hours_worked: 10}
+  end
+
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OL"} = work_assignment) do
+    # Assumption for now -- 0 hours for OL day
+    %{work_assignment | hours_worked: 0}
+  end
+
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OLP"} = work_assignment) do
+    # Assumption for now -- 0 hours for OLPT day
+    %{work_assignment | hours_worked: 0}
+  end
+
+  defp work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OFF"} = work_assignment) do
+    %{work_assignment | hours_worked: 0}
+  end
+
+  defp work_assignment_with_hours(
+         %Draft.WorkAssignment{
+           duty_internal_id: duty_internal_id,
+           roster_set_internal_id: roster_set_internal_id,
+           job_class: job_class,
+           operating_date: date
+         } = work_assignment
+       ) do
+    work_ratio =
+      Draft.RosterDay.work_ratio_for_duty(roster_set_internal_id, duty_internal_id, date)
+
+    %{
+      work_assignment
+      | hours_worked: Draft.JobClassHelpers.num_hours_per_day(job_class, work_ratio)
     }
   end
 
--- lib/draft/work_assignment_setup.ex
+++ lib/draft/work_assignment_setup.ex
@@ -1,6 +1,6 @@
 defmodule Draft.WorkAssignmentSetup do
   @moduledoc """
-  Import the given file specifying the work that each operator is performing on every day of a rating period. Calculate the hours each operator is expected to work each day based on their given job class, assignment, duty id from the file and roster day data already present in the database.
+  Import the given file specifying the work that each operator is performing on every day of a rating period, with the hours each operator is expected to work each day calculated based on their job class & assignment.
   """
   require Logger
 
@@ -8,17 +8,13 @@ defmodule Draft.WorkAssignmentSetup do
           {:ok, Draft.WorkAssignment.t()}
           | {:error, any()}
   @doc """
-  Store the work assignments from the given file in the database with the calculated # of hours
-  to be worked each day.
+  Store the work assignments from the given file in the database with the calculated # of hours to be worked each day.
   """
   def setup(work_file_path \\ "../../data/latest/work_20210811_0128.txt") do
     work_assignments =
       work_file_path
       |> Draft.ParsingHelpers.parse_pipe_separated_file()
       |> Enum.map(&Draft.Parsable.from_parts(Draft.WorkAssignment, &1))
-      # Filtering out VR for now -- should get this file AFTER VR pick has been completed
-      |> Enum.filter(&(&1.assignment != "VR"))
-      |> Enum.map(&work_assignment_with_hours(&1))
 
     Draft.Repo.transaction(fn ->
       _deleted_records =
@@ -29,49 +25,5 @@ defmodule Draft.WorkAssignmentSetup do
 
       Enum.each(work_assignments, &Draft.Repo.insert(&1))
     end)
-  end
-
-  @spec work_assignment_with_hours(Draft.WorkAssignment.t()) :: Draft.WorkAssignment.t()
-  @doc """
-  Return a work assignment with the `hours_worked` field populated
-  based on the job class, assignment, and duty.
-  """
-  def work_assignment_with_hours(%Draft.WorkAssignment{assignment: "LR08"} = work_assignment) do
-    %{work_assignment | hours_worked: 8}
-  end
-
-  def work_assignment_with_hours(%Draft.WorkAssignment{assignment: "LR10"} = work_assignment) do
-    %{work_assignment | hours_worked: 10}
-  end
-
-  def work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OL"} = work_assignment) do
-    # Assumption for now -- 0 hours for OL day
-    %{work_assignment | hours_worked: 0}
-  end
-
-  def work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OLP"} = work_assignment) do
-    # Assumption for now -- 0 hours for OLPT day
-    %{work_assignment | hours_worked: 0}
-  end
-
-  def work_assignment_with_hours(%Draft.WorkAssignment{assignment: "OFF"} = work_assignment) do
-    %{work_assignment | hours_worked: 0}
-  end
-
-  def work_assignment_with_hours(
-        %Draft.WorkAssignment{
-          duty_internal_id: duty_internal_id,
-          roster_set_internal_id: roster_set_internal_id,
-          job_class: job_class,
-          operating_date: date
-        } = work_assignment
-      ) do
-    work_ratio =
-      Draft.RosterDay.work_ratio_for_duty(roster_set_internal_id, duty_internal_id, date)
-
-    %{
-      work_assignment
-      | hours_worked: Draft.JobClassHelpers.num_hours_per_day(job_class, work_ratio)
-    }
   end
 end
