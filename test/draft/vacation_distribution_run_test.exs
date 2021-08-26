15c212bfee31ad314be9e24a6096bf4bf6ec022e
Brady <kbrady@CTD24877KBRADY.local>
2021-08-26 14:27:36 -0400

    test: last_distributed_group

--- test/draft/vacation_distribution_run_test.exs
+++ test/draft/vacation_distribution_run_test.exs
@@ -1,6 +1,7 @@
 defmodule Draft.VacationDistributionRunTest do
   use ExUnit.Case
   use Draft.DataCase
+  import Draft.Factory
   alias Draft.VacationDistributionRun
 
   describe "insert/1" do
@@ -48,6 +49,36 @@ defmodule Draft.VacationDistributionRunTest do
       assert is_nil(
                Draft.Repo.one!(from r in VacationDistributionRun, where: r.id == ^run_id_2).end_time
              )
+    end
+  end
+
+  describe "last_distributed_group/1" do
+    test "Returns nil when no distributions yet" do
+      round = insert!(:round, %{process_id: "process_id", round_id: "round_id"})
+      assert nil == VacationDistributionRun.last_distributed_group(round)
+    end
+
+    test "Returns latest group when multiple" do
+      round = insert!(:round, %{process_id: "process_id", round_id: "round_id"})
+      group1 = insert!(:group, %{process_id: "process_id", round_id: "round_id", group_number: 1})
+      group2 = insert!(:group, %{process_id: "process_id", round_id: "round_id", group_number: 2})
+
+      run_1 = VacationDistributionRun.insert(group1)
+      VacationDistributionRun.mark_complete(run_1)
+      run_2 = VacationDistributionRun.insert(group2)
+      VacationDistributionRun.mark_complete(run_2)
+      assert 2 = VacationDistributionRun.last_distributed_group(round)
+    end
+
+    test "Does not return group where distribution run hasn't completed" do
+      round = insert!(:round, %{process_id: "process_id", round_id: "round_id"})
+      group1 = insert!(:group, %{process_id: "process_id", round_id: "round_id", group_number: 1})
+      group2 = insert!(:group, %{process_id: "process_id", round_id: "round_id", group_number: 2})
+
+      run_1 = VacationDistributionRun.insert(group1)
+      VacationDistributionRun.mark_complete(run_1)
+      VacationDistributionRun.insert(group2)
+      assert 1 = VacationDistributionRun.last_distributed_group(round)
     end
   end
 end
