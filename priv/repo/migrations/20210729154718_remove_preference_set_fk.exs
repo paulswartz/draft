54a6df45d7e17899d142e9ff336faa2fc463fb50
Paul Swartz <paul@paulswartz.net>
2021-09-16 09:29:07 -0400

    fix: support rolling back all migrations
    This allows recreating the database from scratch, without dropping the whole
    database first.

--- priv/repo/migrations/20210729154718_remove_preference_set_fk.exs
+++ priv/repo/migrations/20210729154718_remove_preference_set_fk.exs
@@ -1,10 +1,15 @@
 defmodule Draft.Repo.Migrations.RemovePreferenceSetFk do
   use Ecto.Migration
 
-  def change do
+  def up do
     drop constraint(
            :employee_vacation_preference_sets,
            "employee_vacation_preference_sets_round_id_fkey"
          )
+  end
+
+  def down do
+    # unlikely to do this in practice, so not bothering to recreate the foreign key constraint -ps
+    :ok
   end
 end
--- priv/repo/migrations/20210729191433_consistent_timestamp_type.exs
+++ priv/repo/migrations/20210729191433_consistent_timestamp_type.exs
@@ -1,7 +1,7 @@
 defmodule Draft.Repo.Migrations.ConsistentTimestampType do
   use Ecto.Migration
 
-  def change do
+  def up do
     alter table(:division_vacation_week_quotas) do
       modify :inserted_at, :timestamptz
       modify :updated_at, :timestamptz
@@ -20,6 +20,28 @@ defmodule Draft.Repo.Migrations.ConsistentTimestampType do
     alter table(:employee_vacation_selections) do
       modify :inserted_at, :timestamptz
       modify :updated_at, :timestamptz
+    end
+  end
+
+  def down do
+    alter table(:division_vacation_week_quotas) do
+      modify :inserted_at, :timestamp
+      modify :updated_at, :timestamp
+    end
+
+    alter table(:division_vacation_day_quotas) do
+      modify :inserted_at, :timestamp
+      modify :updated_at, :timestamp
+    end
+
+    alter table(:employee_vacation_quotas) do
+      modify :inserted_at, :timestamp
+      modify :updated_at, :timestamp
+    end
+
+    alter table(:employee_vacation_selections) do
+      modify :inserted_at, :timestamp
+      modify :updated_at, :timestamp
     end
   end
 end
